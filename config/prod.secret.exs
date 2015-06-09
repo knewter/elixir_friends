80183ece60caa0870012b867d810d9ced6b96baf
Josh Adams <josh@isotope11.com>
2015-06-09 17:54:59 -0500

    Add in prod.secret.exs, since we're going to ignore it anyway (heroku)

---
+++ config/prod.secret.exs
@@ -0,0 +1,15 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you likely want to automate and keep it away from
+# your version control system.
+config :elixir_friends, ElixirFriends.Endpoint,
+  secret_key_base: "zM/nNR/YYc6PjmKKDPSK95A9w4TqvrWI3LWFkTCFt3fs1MrkypBjGzz66bXqFYhK"
+
+# Configure your database
+config :elixir_friends, ElixirFriends.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: "postgres",
+  password: "postgres",
+  database: "elixir_friends_prod",
+  size: 20 # The amount of database connections in the pool
