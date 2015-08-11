3c3a1b741d03c234056c706e15f6d669364a3b61
Josh Adams <josh@isotope11.com>
2015-08-11 17:28:04 -0500

    Add react to list posts from the API

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -10,4 +10,59 @@ import {Socket} from "phoenix"
 let App = {
 }
 
+let PostList = React.createClass({
+  getInitialState() {
+    return {
+      posts: []
+    }
+  },
+  componentDidMount() {
+    $.get(this.props.source, result => {
+      result = JSON.parse(result)
+      this.setState({
+        posts: result.entries
+      })
+    })
+  },
+  render() {
+    return(
+      <div className="ui grid stackable">
+        {this.state.posts.map(function(post){
+          return <Post imageUrl={post.image_url} username={post.username} insertedAt={post.inserted_at} content={post.content} />
+        })}
+      </div>
+    )
+  }
+})
+
+let Post = React.createClass({
+  render() {
+    return(
+      <div className="four wide column">
+        <div className="ui card">
+          <div className="image">
+            <img src={this.props.imageUrl} />
+          </div>
+          <div className="content">
+            <div className="header">
+              {this.props.username}
+            </div>
+            <div className="meta">
+              <span className="date">{this.props.insertedAt}</span>
+            </div>
+            <div className="description">
+              {this.props.content}
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+})
+
+window.onload = () => {
+  let element = document.getElementById("app")
+  React.render(<PostList source="/api/posts" />, element)
+}
+
 export default App
