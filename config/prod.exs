fa44b7b0330abe53c6fe90a398c06c35fb9ddda2
Josh Adams <josh@isotope11.com>
2015-10-30 05:59:01 -0500

    Fix syntax error in prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ use Mix.Config
 config :elixir_friends, ElixirFriends.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [host: "elixirfriends.com"],
-  check_origin: ["http://elixirfriends.com", "http://www.elixirfriends.com"]
+  check_origin: ["http://elixirfriends.com", "http://www.elixirfriends.com"],
   cache_static_manifest: "priv/static/manifest.json"
 
 # ## SSL Support
