1d82f869b61f6da3d547da1f669fb9a8fe2a37db
Josh Adams <josh.rubyist@gmail.com>
2015-12-19 08:26:55 -0600

    Downgrade phoenix to 1.0.4

--- mix.exs
+++ mix.exs
@@ -40,7 +40,7 @@ defmodule ElixirFriends.Mixfile do
   # Type `mix help deps` for examples and options
   defp deps do
     [
-      {:phoenix, "~> 1.0"},
+      {:phoenix, "~> 1.0.0"},
       {:phoenix_ecto, "~> 1.0.0"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.1.0"},
